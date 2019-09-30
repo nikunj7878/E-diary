f1295ffbea0ebe0bdd6c5b70ae7f9fcb04ea48c4
user <vaghaninikunj24@gmail.com>
2019-09-30 11:56:03 +0300

    20190930_11_55

---
+++ works/hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ // this lirary shold be added for printf to work
+void main()
+ {
+// Hello world!;// absolutely wrong!
+ "Hello world!"; // working, but text doesn't appear on screen
+ printf("Hello wrold!\n");
+ }
